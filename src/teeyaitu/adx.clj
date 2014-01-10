3a3758bea81423f2baae0c34423fab1ffcc91ebc
ae589 <damnpanache@gmail.com>
2014-01-10 22:00:35 +0000

    Still fixing rounding errors

--- src/teeyaitu/adx.clj
+++ src/teeyaitu/adx.clj
@@ -18,10 +18,12 @@
 (defn abs [x]
   (if (< x 0) (* x -1) x))
 
+(defn div2dp [x y] (with-precision 2 (/ x y)))
+
 (defn calc-tr [curr-hi curr-lo prev-close]
   (let [hi-less-lo (- curr-hi curr-lo)
-        hi-less-close (- curr-hi prev-close)
-        lo-less-close (- curr-lo prev-close)]
+        hi-less-close (abs (- curr-hi prev-close))
+        lo-less-close (abs (- curr-lo prev-close))]
     (max (max hi-less-lo hi-less-close) lo-less-close)))
 
 (defn calc-plus-dm [prev-hi prev-lo curr-hi curr-lo]
@@ -47,14 +49,14 @@
       (assoc :-DM (calc-minus-dm (prev :high) (prev :low) (current :high) (current :low)))))
 
 (defn calc-di-and-dx [curr]
-  (let [plus-di14 (with-precision 2 (* 100 (/ (curr :+DM14) (curr :TR14))))
-        minus-di14 (with-precision 2 (* 100 (/ (curr :-DM14) (curr :TR14))))
+  (let [plus-di14 (with-precision 2 (* 100 (div2dp (curr :+DM14) (curr :TR14))))
+        minus-di14 (with-precision 2 (* 100 (div2dp (curr :-DM14) (curr :TR14))))
         di14-diff (abs (- plus-di14 minus-di14))
         di14-sum (+ plus-di14 minus-di14)]
     (-> curr
         (assoc :+DI14 plus-di14)
         (assoc :-DI14 minus-di14)        
-        (assoc :DX (/ (* 100 di14-diff) di14-sum)))))
+        (assoc :DX (div2dp (* 100 di14-diff) di14-sum)))))
 
 (defn sum-tr-and-dm [summed nextday]
     (-> summed
@@ -65,13 +67,17 @@
 
 (defn calc-tr14-initial [adxs new-day]
   "To be used to calculate the initial value of TR14"
-  (calc-di-and-dx (reduce sum-tr-and-dm new-day adxs)))
+  (let [initial-val (-> new-day
+                        (assoc :TR14 (new-day :TR 0))
+                        (assoc :+DM14 (new-day :+DM 0))
+                        (assoc :-DM14 (new-day :-DM 0)))]
+    (calc-di-and-dx (reduce sum-tr-and-dm initial-val adxs))))
 
 
-(defn calc-single-adx [prev curr]
+(defn calc-single-adx [curr prev]
   (let [prev-adx (prev :ADX 0)]
     (if (> prev-adx 0)
-      (assoc curr :ADX (/ (+ (curr :DX) (* prev-adx 13) ) 14))
+      (assoc curr :ADX (div2dp (+ (curr :DX) (* prev-adx 13) ) 14))
       curr)))
 
 (defn calc-tr14-full [adxs new-day]
@@ -82,22 +88,23 @@
         prev-plus-dm14 (prev :+DM14)
         prev-minus-dm14 (prev :-DM14)]
     (-> current
-        (assoc :TR14 (+ (- prev-tr-14 (/ prev-tr-14 14)) (current :TR)))
-        (assoc :+DM14 (+ (- prev-plus-dm14 (/ prev-plus-dm14 14)) (current :+DM)))
-        (assoc :-DM14 (+ (- prev-minus-dm14 (/ prev-minus-dm14 14)) (current :-DM)))
+        (assoc :TR14 (+ (- prev-tr-14 (div2dp prev-tr-14 14)) (current :TR)))
+        (assoc :+DM14 (+ (- prev-plus-dm14 (div2dp prev-plus-dm14 14)) (current :+DM)))
+        (assoc :-DM14 (+ (- prev-minus-dm14 (div2dp prev-minus-dm14 14)) (current :-DM)))
         calc-di-and-dx
-        (calc-single-adx prev current)
+        (calc-single-adx prev)
         )))
 
 (defn set-initial-adx [adxs current]
-  (assoc current :ADX (/ (reduce #(+ %1 (%2 :high)) 0 adxs) 14)))
+  (assoc current :ADX (div2dp (reduce #(+ %1 (%2 :high)) 0 adxs) 14)))
 
 (defn calc-adx [adxs new-day]
   (let [so-far (count adxs)
         values-required 14]
     (cond (= so-far (* 2 values-required)) (conj adxs (calc-tr14-full adxs (set-initial-adx adxs new-day)))
             (> so-far values-required) (conj adxs (calc-tr14-full adxs new-day))
-            (= so-far values-required) (conj adxs (calc-tr14-initial adxs new-day))
+            (= so-far values-required)
+            (conj adxs (calc-tr14-initial adxs                                                                     (calc-tr-and-dm (last adxs) new-day)))
             (= so-far 0) (conj adxs new-day)
-            (true) (conj adxs (calc-tr-and-dm (last adxs) new-day)))))
+            (= 1 1) (conj adxs (calc-tr-and-dm (last adxs) new-day)))))
 
--- test/teeyaitu/adx_test.clj
+++ test/teeyaitu/adx_test.clj
@@ -4,37 +4,52 @@
         clojure-csv.core
         ))
 
-(defonce test-vals
+(def test-vals
   (parse-csv (clojure.string/replace
               (slurp "test/cs-adx.csv") "\r" "\n")))
 
+(defn to-bigdec [x]
+  (with-precision 10 :rounding HALF_UP (bigdec x)))
+
 (def test-days
   (map #(-> {
-             :high (first %)
-             :low (second %)
-             :close (nth % 2)} )
+             :high (to-bigdec (first %))
+             :low (to-bigdec (second %))
+             :close (to-bigdec (nth % 2))} )
        test-vals))
 
-(def test-adxs (map calc-adx [] test-days))
+(def test-adxs (reduce calc-adx [] test-days))
 
 (defn day [high low close]
   {:high high :low low :close close})
 
-(def day1 (day 44.53M 43.98M 44.52M))
-(def day2 (day 44.93M 44.36M 44.65M))
-(def day3 (day 45.39M 44.70M 45.22M))
+(def day-a1 (day 44.53M 43.98M 44.52M))
+(def day-a2 (day 44.93M 44.36M 44.65M))
+(def day-a3 (day 45.39M 44.70M 45.22M))
 
-(fact "Calculates correct TR for two days"
-      (let [calc (calc-tr-and-dm day1 day2)]
+(def day-b1 (day 30.20M	29.41M 29.87M))
+(def day-b2 (day 30.28M	29.32M 30.24M))
+(def day-b3 (day 30.45M	29.96M 30.10M))
+(def day-b4 (day 29.35M	28.74M	28.90M))
+
+
+(fact "Calculates correct TR for two days 1"
+      (let [calc (calc-tr-and-dm day-a1 day-a2)]
         (calc :TR)) => 0.57M)
+(fact "Calculates correct TR for two days 2"
+      (let [calc (calc-tr-and-dm day-b2 day-b3)]
+        (calc :TR)) => 0.49M)
+(fact "Calculates correct TR for two days 3"
+      (let [calc (calc-tr-and-dm day-b3 day-b4)]
+        (calc :TR)) => 1.36M)
 
 (fact "Sums previous TR and DM values"
-      (let [adxs [day1 (calc-tr-and-dm day1 day2)]
-            summed (calc-tr14-initial adxs day3)]
+      (let [adxs [day-a1 (calc-tr-and-dm day-a1 day-a2)]
+            summed (calc-tr14-initial adxs day-a3)]
         (summed :TR14)
         ) => 0.57M)
 
 (fact "ADX value 27 should be 33.58"
-      ((nth test-adxs 27) :ADX) => 33.58)
+      ((nth test-adxs 28) :ADX) => 33.58)
 
 
