2ab2fec2e9ae8c7e2cc41cc7cb9343c9e2cd5f04
ae589 <damnpanache@gmail.com>
2014-01-11 21:13:17 +0000

    Add comments for future work

--- src/teeyaitu/core.clj
+++ src/teeyaitu/core.clj
@@ -4,13 +4,14 @@
 (use 'incanter.core)
 (import [java.net URL])
 
+;;For each stock examine the last 7 days for
+;; ADX>30
+;; 12 month high
+;; outperfoming the FTSE by 15%
 
+;;For this sublist find stocks
+;; That have had a 2 month high/low between 3 and 7 days ago
+;; Is now lower/higher that that value
 
-
-
-
-
-
-
-
-
+;;For this sublist, find stocks that go through the previous days
+;; high/low
