7ac445243c65cbfdc6ac92c01738a496293e2341
Dean Pehrsson-Chapman <dean@p14n.com>
2014-01-26 13:24:31 +0000

    Downloading and parsing 350 complete

---
+++ src/teeyaitu/data.clj
@@ -0,0 +1,56 @@
+(ns teeyaitu.data
+  (:use teeyaitu.core
+        clojure-csv.core))
+(require '(net.cgrand [enlive-html :as html]))
+
+;;components
+;;http://uk.finance.yahoo.com/q/cp?s=%5EFTSE&c=0 1 2
+;;http://uk.finance.yahoo.com/q/cp?s=%5EFTMC&c=0 1 2 3 4 5
+(def ftse100root "http://uk.finance.yahoo.com/q/cp?s=%5EFTSE&c=")
+(def ftse250root "http://uk.finance.yahoo.com/q/cp?s=%5EFTMC&c=")
+
+(defn fetch-from-url[address]
+  (with-open [stream (.openStream (java.net.URL. address))]
+    (let  [buf (java.io.BufferedReader. 
+                (java.io.InputStreamReader. stream))]
+      (clojure.string/join "\n" (line-seq buf)))))
+
+(defn fetch-and-save-prices[stock]
+  (try 
+    (spit (str "data/" stock ".csv")
+          (fetch-url (str "http://ichart.finance.yahoo.com/table.csv?s=" stock "&d=0&e=23&f=2014&g=d&a=4&b=24&c=1999&ignore=.csv")))
+    (catch Exception e (println (str "Couldn't get " stock " " (.getMessage e))))))
+
+(defn html-from-file[x]
+  (html/html-resource (java.net.URL. (str "file:" x))))
+
+(defn tickers-from-index-page [html]
+  (map #(first (:content %))  (html/select html [:td.yfnc_tabledata1 :a])))
+
+(defn tickers-from-url [url]
+  (tickers-from-index-page (html/html-resource (java.net.URL. url))))
+
+(defn fetch-350-tickers []
+  (flatten (pmap tickers-from-url
+                 [(str ftse100root "0")
+                   (str ftse100root "1")
+                   (str ftse100root "2")
+                   (str ftse250root "0")
+                   (str ftse250root "1")
+                   (str ftse250root "2")
+                   (str ftse250root "3")
+                   (str ftse250root "4")
+                   (str ftse250root "5")])))
+
+(defn save-stock-prices [stocks]
+  (pmap fetch-and-save-prices stocks))
+
+(defn load-values-from-csv [stock]
+  (filter #(not (= "000" (nth % 5)))
+          (reverse (rest (parse-csv (clojure.string/replace
+                                     (slurp (str "data/" stock ".csv")) "-" ""))))))
+
+(defn csv-prices-to-day-map [csv-prices] (map #(-> {:date (Integer/parseInt (first %))
+                                          :high (to-bigdec (nth % 2))
+                                          :low (to-bigdec (nth % 3))
+                                          :close (to-bigdec (nth % 4))}) csv-prices))
---
+++ src/teeyaitu/trade.clj
@@ -0,0 +1,26 @@
+(ns teeyaitu.trade
+  (:use teeyaitu.data
+        teeyaitu.core
+                teeyaitu.adx
+        clojure-csv.core))
+
+(defn save-350-to-disk
+  ([] (save-350-to-disk 350 (fetch-350-tickers)))
+  ([last-x all-tickers]
+   (map fetch-and-save-prices (take-last last-x (all-tickers)))))
+
+(defn get-saved-data-stocks []
+  (map
+   #(let [name (.getName %)] (.substring name 0 (- (.length name) 4)))
+   (seq (.listFiles (java.io.File. "data/")))))
+
+(defn faky-one [stock ftse-3m-vals]
+  (let [day-prices (csv-prices-to-day-map (load-values-from-csv stock))
+        adx-values (reduce calc-adx [] day-prices)]
+    (try (faky-trady stock adx-values ftse-3m-vals)
+         (catch Exception e (println (str "Couldn't calc trades on " stock " " (.toString e)))))
+    ))
+
+(defn faky-all [ftse-3m-vals]
+  (map #(faky-one % ftse-3m-vals) (get-saved-data-stocks)))
+
